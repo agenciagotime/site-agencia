776d81d3bfbd84211a666555776e88d5948fd318
Kelvin Santiago <kelvinsleonardo@gmail.com>
2018-05-31 16:40:34 -0300

    Melhorias UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,12 @@ let menuItems = document.querySelectorAll('.menu-item');
 let menuSocial = document.querySelector('.menu-social');
 let loader = document.getElementById('loader');
 
-
 window.onload = function () {
   setTimeout(function(){
     loader.classList.add('hide');
-},1200); 
-  
-}
+},1200);
+
+};
 
 btnMenu.addEventListener('click', function() {
   if (btnMenu.classList.contains('open')) {
@@ -33,21 +32,20 @@ btnMenu.addEventListener('click', function() {
     for (let i = 0; i < menuItems.length; i++) {
       menuItems[i].classList.add('show');
     }
-  }  
+  }
 });
 
-function checkMail(form) {
-  let mail = document.getElementById('email');
+function checkMail(element) {
   let error = document.getElementById('error');
-  if (!mail.validity.valid) {
+  if (!element.validity.valid) {
     error.classList.add('alert');
-    form.focus();
+      element.focus();
   } else {
     error.classList.remove('alert');
-  }  
-  form.preventDefault();
+  }
 }
 
+// Open modal request proposal
 function openModal(e) {
   modal.classList.add('active');
 }
@@ -68,3 +66,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     body.classList.remove('no-scroll');
   });
 });
+
+function submitForm() {
+    var frm = document.getElementsByName('form-request-propose')[0];
+    document.getElementById('submit-hidden').click();
+
+    if (frm.checkValidity()) {
+        frm.reset();
+    }
+    return false;
+}
